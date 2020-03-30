1146f26e832502e1aea2ad7d895d0ce5d8611a7d
Jesús <jesusm_g@msn.com>
2020-03-30 15:16:42 +0200

    valor por defecto en los selects

--- js/codigo.js
+++ js/codigo.js
@@ -183,6 +183,7 @@ function select_transport() {
         if (ajax3.readyState == 4 && ajax3.status == 200) {
             var respuesta=JSON.parse(this.responseText);
             for (var i = 0; i < respuesta.length; i++) {
+            option += '<option value="0">Escollir una opció</option>';
             option += '<option value="' + respuesta[i].id_nom_transport+'">' + respuesta[i].nom_transport+'</option>';
             console.log(option);
             }
