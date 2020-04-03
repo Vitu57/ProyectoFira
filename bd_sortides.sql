40389d5c953b1600fa33adc912016f75b4005e88
AlumnoDeDawN04 <55807836+AlumnoDeDawN04@users.noreply.github.com>
2020-04-03 19:02:10 +0200

    Base de datos con casos

--- bd_sortides.sql
+++ bd_sortides.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-04-2020 a las 16:17:27
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.3
+-- Tiempo de generación: 03-04-2020 a las 19:01:12
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -45,11 +45,10 @@ CREATE TABLE `tbl_activitat` (
 --
 
 INSERT INTO `tbl_activitat` (`id_activitat`, `nom_activitat`, `lloc_activitat`, `tipus_activitat`, `ambit_activitat`, `jornada_activitat`, `objectiu_activitat`, `id_contacte_activitat`, `id_sortida`) VALUES
-(1, 'Salida al campo', 'El campo', 'Curricular', 'Nacional', 'Mati', 'Andar', 1, 1),
-(2, 'Salida al cine', 'Splau', 'No curricular', 'Nacional', 'Tarda', 'Ver una peli', 1, 2),
-(3, 'Visita al museo', 'MNAC', 'Curricular', 'Internacional', 'Sencera', 'Ver un museo', 1, 3),
-(4, 'Colonies', 'Can Joval', 'Curricular', 'Nacional', 'Sencera', 'Que se muevan y nos dejen en paz', 1, 4),
-(11, '', '', '', '', 'Tarda', '', 8, 12);
+(2, 'Salida el campo', 'Campo ', 'No curricular', 'Nacional', 'Sencera', 'Pasear', 3, 7),
+(3, 'Visita al CERN', 'CERN', 'Curricular', 'Internacional', 'Sencera', 'Visitar el CERN', 4, 8),
+(4, 'Visita al acuario', 'Acuario', 'Curricular', 'Nacional', 'Mati', 'Veure l\'aquari', 5, 9),
+(5, 'Sortida a la neu', 'Molina', 'No curricular', 'Nacional', 'Sencera', 'Que els alumnes disfrutin de la neu', 6, 10);
 
 -- --------------------------------------------------------
 
@@ -63,16 +62,6 @@ CREATE TABLE `tbl_asistencia` (
   `id_usuario` int(11) NOT NULL,
   `id_activitat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `tbl_asistencia`
---
-
-INSERT INTO `tbl_asistencia` (`id_asistencia`, `estado_asistencia`, `id_usuario`, `id_activitat`) VALUES
-(1, 'Presente', 1, 1),
-(2, 'Ausente', 2, 2),
-(3, 'Presente', 3, 3),
-(4, 'Ausente', 4, 4);
 
 -- --------------------------------------------------------
 
@@ -140,14 +129,10 @@ CREATE TABLE `tbl_contacte_activitat` (
 --
 
 INSERT INTO `tbl_contacte_activitat` (`id_contacte_activitat`, `persona_contacte`, `web_contacte`, `telefon_contacte`, `email_contacte`) VALUES
-(1, 'persona de contacto', 'www.webdecontacto.com', 123456789, 'email_contacto@gmail.com'),
-(2, '', '', 0, ''),
-(3, 'Juanma', 'www.juanma123123adwl.com', 622738738, 'email232133123@gmail.com'),
-(4, '', '', 0, ''),
-(5, '', '', 0, ''),
-(6, '', '', 0, ''),
-(7, '', '', 0, ''),
-(8, '', '', 0, '');
+(3, 'persona', 'www.dgdf.com', 123456789, 'kbcbjsd@gmail.com'),
+(4, 'trabajador de CERN', 'cern.com', 214354657, 'cern@gmail.com'),
+(5, 'contacte', 'aqua.com', 987654321, 'aqua@gmail.com'),
+(6, 'instructor', 'lamolina.com', 765432167, 'lamolina@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -195,7 +180,9 @@ INSERT INTO `tbl_lista_profesores` (`id_lista_profesores`, `id_profesor`, `id_ex
 (2, 2, 2),
 (3, 28, 2),
 (4, 29, 1),
-(5, 2, 7);
+(5, 1, 6),
+(6, 2, 7),
+(7, 28, 8);
 
 -- --------------------------------------------------------
 
@@ -247,18 +234,10 @@ CREATE TABLE `tbl_preus` (
 --
 
 INSERT INTO `tbl_preus` (`id_preus`, `cost_substitucio`, `cost_activitat_individual`, `cost_extra_activitat_profe`, `cost_global_activitat`, `cost_final`, `preu_fixe`, `preu_sense_topal`, `preu_amb_topal`, `preu_gestio`, `overhead`, `total_facturar`, `pagament_fraccionat`, `observacio_fraccionat`) VALUES
-(1, 5, 1, 2, 4, 3, 4, 5, 3, 2, 23, 5, 'si', 'ejemplo'),
-(2, 5, 1, 2, 4, 3, 4, 5, 3, 2, 23, 5, 'si', 'ejemplo'),
-(3, 5, 1, 2, 4, 3, 4, 5, 3, 2, 23, 5, 'si', 'ejemplo'),
-(4, 5, 1, 2, 4, 3, 4, 5, 3, 2, 23, 5, 'si', 'ejemplo'),
-(5, 5, 1, 2, 4, 3, 4, 5, 3, 2, 23, 5, 'si', 'ejemplo'),
-(6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'si', ''),
-(7, 23, 21, 23, 42, 323, 123, 543, 231, 123, 10, 1231, 'no', 'Observacion Costos'),
-(8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'si', ''),
-(9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'si', ''),
-(10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'si', ''),
-(11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'si', ''),
-(12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'si', '');
+(7, 564, 456, 465564, 654, 54, 564, 54, 564, 654, 54, 546, 'si', 'Observacion'),
+(8, 567, 56, 6, 65, 65, 56, 65, 67, 6, 6, 56, 'si', 'Observaciones '),
+(9, 87, 78, 78, 87, 87, 78, 87, 78, 78, 78, 78, 'si', 'Mes observacions'),
+(10, 10, 10, 101, 10, 1010, 10, 10, 101, 10, 10, 1, 'si', 'Mes obsevacions d\'exemple');
 
 -- --------------------------------------------------------
 
@@ -288,13 +267,10 @@ CREATE TABLE `tbl_sortida` (
 --
 
 INSERT INTO `tbl_sortida` (`id_sortida`, `codi_sortida`, `inici_sortida`, `final_sortida`, `observacions_sortida`, `numero_alumnes`, `n_vetlladors`, `n_acompanyants`, `profes_a_part`, `profesor_asignat`, `id_clase`, `id_transport`, `id_precios`, `comanda_menu`) VALUES
-(1, 'R-P-0015', '2020-03-29', '2020-03-23', 'Una salida muy chachi pistachi', 50, 3, 0, 2, 'MCarpallo', 1, 1, 1, 0),
-(2, 'R-P-0012', '2020-03-23', '0000-00-00', 'Una salida muy chachi', 60, 2, 2, 0, 'JCarcedo', 21, 2, 5, 0),
-(3, 'R-P-0035', '2020-03-23', '0000-00-00', 'Una salida muy pistachi', 0, 5, 3, 4, 'SGimenez', 3, 3, 2, 0),
-(4, 'R-P-0014', '2020-03-23', '0000-00-00', 'Una salida muy chachi pistachi', 0, 3, 5, 2, 'JCarcedo', 4, 4, 3, 0),
-(5, 'R-P-0055', '2020-03-24', '0000-00-00', 'Una salida muy marabitupenda', 0, 3, 0, 2, 'MCarpallo', 10, 5, 4, 0),
-(7, 'JJk32', '2020-04-01', '2020-04-01', 'Observacion salida test', 45, 2, 2, 0, 'Mario Carpallo', 27, 7, 7, 0),
-(12, '', '2020-04-06', '2020-04-07', '', 0, 0, 0, 0, '', 8, 12, 12, 0);
+(7, '1234', '2020-04-03', '2020-04-03', 'Observacion Okay', 4, 2, 2, 1, 'JCarcedo', 2, 7, 7, 0),
+(8, '1', '2020-04-04', '2020-04-06', 'Observacion', 23, 0, 0, 1, 'DLarrea', 21, 8, 8, 0),
+(9, '3', '2020-04-14', '2020-04-14', 'Altres observacions', 10, 6, 5, 2, 'MCarpallo', 1, 9, 9, 0),
+(10, 'S-27', '2020-05-01', '2020-05-03', 'Varis profes', 120, 0, 0, 5, 'MCarpallo', 18, 10, 10, 0);
 
 -- --------------------------------------------------------
 
@@ -344,16 +320,10 @@ CREATE TABLE `tbl_transport` (
 --
 
 INSERT INTO `tbl_transport` (`id_transport`, `hora_sortida`, `hora_arribada`, `cost_transport`, `codi_contacte`, `comentaris_transport`, `id_nom_transport`) VALUES
-(1, '02:00', '09:00', 0, 1, 'Coche de san fernando lalalala', 1),
-(2, '00:00:', '00:00:', 10, 1, 'Tren chuchu', 2),
-(3, '00:00:', '00:00:', 25, 1, 'Autocar al battle royale', 3),
-(4, '00:00:', '00:00:', 100, 1, 'Avion contaminador', 4),
-(5, '00:00:', '00:00:', 3, 1, 'Coronavirus Coronavirus', 5),
-(6, '', '', 0, 0, '', 1),
-(7, '15:00', '18:00', 23, 2313, 'Comentario Transporte', 2),
-(10, '', '', 0, 0, '', 2),
-(11, '', '', 0, 0, '', 3),
-(12, '', '', 0, 0, '', 2);
+(7, '10:00', '19:00', 10, 123, 'Comentario', 3),
+(8, '08:00', '08:00', 50, 5, 'Salida de mas de un dia', 4),
+(9, '09:00', '17:00', 5, 654, 'Comentarios varios', 3),
+(10, '08:00', '21:00', 20, 9, 'Comentari d\'exemple', 3);
 
 -- --------------------------------------------------------
 
@@ -490,13 +460,13 @@ ALTER TABLE `tbl_usuari`
 -- AUTO_INCREMENT de la tabla `tbl_activitat`
 --
 ALTER TABLE `tbl_activitat`
-  MODIFY `id_activitat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_activitat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_asistencia`
 --
 ALTER TABLE `tbl_asistencia`
-  MODIFY `id_asistencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_asistencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_clase`
@@ -508,7 +478,7 @@ ALTER TABLE `tbl_clase`
 -- AUTO_INCREMENT de la tabla `tbl_contacte_activitat`
 --
 ALTER TABLE `tbl_contacte_activitat`
-  MODIFY `id_contacte_activitat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_contacte_activitat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_etapa`
@@ -520,7 +490,7 @@ ALTER TABLE `tbl_etapa`
 -- AUTO_INCREMENT de la tabla `tbl_lista_profesores`
 --
 ALTER TABLE `tbl_lista_profesores`
-  MODIFY `id_lista_profesores` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_lista_profesores` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_nom_transport`
@@ -532,13 +502,13 @@ ALTER TABLE `tbl_nom_transport`
 -- AUTO_INCREMENT de la tabla `tbl_preus`
 --
 ALTER TABLE `tbl_preus`
-  MODIFY `id_preus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_preus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_sortida`
 --
 ALTER TABLE `tbl_sortida`
-  MODIFY `id_sortida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_sortida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_tipus_usuari`
@@ -550,7 +520,7 @@ ALTER TABLE `tbl_tipus_usuari`
 -- AUTO_INCREMENT de la tabla `tbl_transport`
 --
 ALTER TABLE `tbl_transport`
-  MODIFY `id_transport` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_transport` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_usuari`
