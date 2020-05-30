68d3ea62c01166bf825c0da9bfdc8a9afeae32ec
jaimecll <jaimecgfg@gmail.com>
2020-05-30 15:22:07 +0200

    cambiar apellidos

--- services/cambiar_clase.proc.php
+++ services/cambiar_clase.proc.php
@@ -5,8 +5,12 @@ $id=$_REQUEST['id_alumno'];
 $clase=$_REQUEST['id_clase'];
 $nom=$_REQUEST['nom'];
 $cognoms=$_REQUEST['cognom'];
-$cognom1="pepe";
-$cognom2="pepe";
+if(str_word_count($cognoms, 0)==2){
+        list($cognom1, $cognom2) = explode(' ', $cognoms);
+        }else{
+            $cognom1 = $cognoms;
+            $cognom2 = "-";
+        }
 
 $consulta="UPDATE `tbl_alumnes` SET `nom_alumne`='$nom',`cognom1_alumne`='$cognom1',`cognom2_alumne`='$cognom2',`id_clase`=$clase WHERE id_alumne=$id";
 $consulta2=mysqli_query($conn,$consulta);
